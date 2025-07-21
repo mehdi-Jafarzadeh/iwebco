f9fee3e56463dd173a5675b1cf3b4969cb1b00ac
mehdi Jafarzadeh <mehdijafarzadehh4@gmail.com>
2025-07-21 18:03:11 +0330

    production fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
           <img src={Logo} className="logo" alt="Webco logo" />
       <h1>Under production</h1>
       <div className="loader"></div>
+      <h2>mehdijafarzadehh4@gmail.com</h2>
+      <h2>+989143259118</h2>
       </div>
     </>
   )
